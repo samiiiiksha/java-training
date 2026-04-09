93488b7c1b3f5582303aba6ab69c02f48aa5ee2c
samiiiiksha <whimzyward@gmail.com>
2026-04-09 18:35:58 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,24 @@
+class Speaker {
+    public static void speak(String message) {
+        System.out.println("Speaker says " + message);
+    }
+
+    public static void speak(String message, int volume) {
+        System.out.println("Speaker says " + message + " with volume " + volume);
+    }
+
+    public static void speak(String message, int volume, String language) {
+        System.out.println("Speaker says " + message + " with volume " + volume + " in " + language);
+    }
+}
+
+public class MethodOverloading{
+
+    public static void main(String[] args) {
+        Speaker speaker = new Speaker();
+
+        Speaker.speak("Hello World");
+        Speaker.speak("Hello World", 5);
+        Speaker.speak("Hello World", 5, "English");
+    }
+}
