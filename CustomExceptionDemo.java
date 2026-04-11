ea5d95f3e3c2633c91d3e415809e20dff8c3da05
samiiiiksha <whimzyward@gmail.com>
2026-04-11 20:02:15 +0530

    Add files via upload

---
+++ CustomExceptionDemo.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+/* Define a custom exception class that extends excception class. 
+    Provide a constructor in the custom exception calss to initialize
+    Throw the custom exception in teh appropriate places.
+    Catch the custom exception using a try-catch block.
+*/
+
+class CustomException extends Exception {
+    public CustomException (String message) {  // constructor
+        super(message);    // super keyword to call the constructor
+    }
+}
+
+public class CustomExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            validateAge(15);
+        } catch (CustomException e) {
+            System.out.println("CAUGHT a custom exception: " + e.getMessage());
+        }
+    }
+
+    public static void validateAge(int age) throws CustomException {
+        if (age < 18) {
+            throw new CustomException("Age must be atleast to register.");
+        } else {
+            System.out.println("You're eligible to register!");
+        }
+    }
+
+}
